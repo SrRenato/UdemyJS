9792b2a2b8c1fd9d459c1efc20c64392c829db4e
SrRenato <renatoa087@gmail.com>
2020-05-14 23:33:23 -0300

    add

--- Exercicios-JS/Exer01.js
+++ Exercicios-JS/Exer01.js
@@ -19,26 +19,5 @@ console.log("O resultado = ",soma)
 console.log("O resultado = ",subtra)
 console.log("O resultado = ",multi)
 console.log("O resultado = ",divi)
-dsdsadasdasfdfa
-=======
-/*01)​ Crie uma função que dado dois valores
- (passados como parâmetros) mostre no console a soma, 
- subtração, multiplicação e divisão desses valores. 
-*/
 
-function Calcular(num1,num2) {
 
-    soma = num1 + num2 
-    subtra = num1 - num2 
-    multi = num1 * num2 
-    divi = num1 / num2 
-
-}
-
-Calcular(5,6)
-
-console.log("O resultado = ",soma)
-console.log("O resultado = ",subtra)
-console.log("O resultado = ",multi)
-console.log("O resultado = ",divi)
->>>>>>> 2dcf73e6c212ac486eb8c0f1d2654d8f6c9222f8
